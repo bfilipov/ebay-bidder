7892217e305a492533c6421906a7f15525a37a7e
bfilipov <filipov.bogomil@gmail.com>
2018-07-22 18:16:47 +0300

    update

---
+++ bidder.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import time
+import ebaySeleniumOperatons as eso
+
+#Globals
+amountForSearch = 0.26 #in usd #double
+actualBidAmmount = 0.32 #in usd #double
+with open('login.txt') as f:
+    #login.txt is included in .gitignore
+    #in login.txt provide #1st line - username,2nd line - password
+    lines = f.readlines()
+emailUsed = lines[0]
+passswordUsed = lines[1]
+#Globals
+
+#chrome webdriver
+driver = webdriver.Chrome()
+#set implicit wait for 1 second.
+driver.implicitly_wait(0)
+#use explicit waits for each element that must be clicked on.
+
+
+#URL LIST
+#https://www.ebay.com/sch/i.html?_odkw=fidget+-cap+-caps+-bearing+-bearings+-weight&_sop=1&_udhi=0.2&_mPrRngCbx=1&LH_FS=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.Xfidget+-cap+-caps+-bearing+-bearings+-weight+-yo.TRS0&_nkw=fidget+-cap+-caps+-bearing+-bearings+-weight+-yo&_sacat=0"
+urls = [
+"https://www.ebay.com/sch/i.html?_odkw=leather+case+samsung+%28s7%2Cs8%2Cj3%2Cj5%2Cj7%29&_sop=1&_udhi=0.6&_mPrRngCbx=1&LH_FS=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.Xleather+case+samsung+%28s7%2Cs8%2Cj3%2Cj5%2Cj7%2Ca3%2Ca5%2Ca7%29.TRS0&_nkw=leather+case+samsung+%28s7%2Cs8%2Cj3%2Cj5%2Cj7%2Ca3%2Ca5%2Ca7%29&_sacat=0", #leather case samsung (s7,s8,j3,j5,j7,a3,a5,a7) pod 0.6
+"https://www.ebay.com/sch/i.html?_odkw=leather+case+iphone++-%22iphone+5%22+-%22iphone+4%22&_sop=1&_udhi=0.6&_mPrRngCbx=1&LH_FS=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.Xleather+case+iphone+%286%2C7%29++-%22iphone+5%22+-%22iphone+4%22.TRS0&_nkw=leather+case+iphone+%286%2C7%29++-%22iphone+5%22+-%22iphone+4%22&_sacat=0", #iphone 6,7 leather case pod 0.6
+"https://www.ebay.com/sch/i.html?_odkw=%28leather%2CPU%2Cflip%29+case+huawei&_sop=1&_udhi=0.6&_mPrRngCbx=1&LH_FS=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.X%28leather%2CPU%2Cflip%29+case+huawei+%28p9%2Cp10%29.TRS0&_nkw=%28leather%2CPU%2Cflip%29+case+huawei+%28p9%2Cp10%29&_sacat=0", #huawei p9,p10 po 0.6
+"https://www.ebay.com/sch/i.html?_odkw=%28leather%2CPU%2Cflip%29+case+%282017%2C2016%2Ciphone%29&_sop=1&_udhi=0.6&_mPrRngCbx=1&LH_FS=1&_osacat=0&_from=R40&_trksid=p2045573.m570.l1313.TR0.TRC0.H0.X%28leather%2CPU%2Cflip%29+case+%282017%2C2016%29.TRS0&_nkw=%28leather%2CPU%2Cflip%29+case+%282017%2C2016%29&_sacat=0", #(leather,PU,flip) case (2017,2016) 
+]
+
+if __name__ == "__main__":
+    eso.signin(driver,emailUsed,passswordUsed)
+    a = eso.findSearchItems(urls,amountForSearch)
+    eso.bidOnItems(driver,a,actualBidAmmount)
---
+++ ebaySeleniumOperatons.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 16:16:56 2018
+
+@author: bFilipov
+"""
+from urllib.request import urlopen
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def signin(driver,emailUsed,passswordUsed):
+    driver.get('https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=http%3A%2F%2Fwww.ebay.com%2F')
+    
+    action1 = webdriver.ActionChains(driver)
+    #action1.move_to_element(driver.find_element_by_css_selector('div #pri_signin div span input')).send_keys(emailUsed)''Stopped woringg 02.2017
+    #action1.move_to_element(driver.find_element_by_css_selector('div #pri_signin input')).send_keys(emailUsed) stopped working 10.2017
+    action1.move_to_element(driver.find_element_by_css_selector('input[placeholder="Email or username"]')).click().send_keys(emailUsed)
+    action1.perform()
+    action2 = webdriver.ActionChains(driver)
+    #action2.move_to_element(driver.find_element_by_css_selector('div #pri_signin :nth-child(6) span input')).click().send_keys(passswordUsed) stopped worlomg 10.2017
+    action2.move_to_element(driver.find_element_by_css_selector('input[type="Password"]')).click().send_keys(passswordUsed)
+    action2.perform()
+    driver.find_element_by_id('sgnBt').click()
+
+def findSearchItems(urls=[],limitAmount=0.05):
+    itemsInQuery = []
+    for item in urls:
+        html=urlopen(item)
+        bsObj = BeautifulSoup(html, "html.parser")
+        nameList = bsObj.findAll("li", {"class":"sresult lvresult clearfix li"})
+        #driver.get(item) commented on 10.2017 to speed things up
+        print(item)
+
+        for name in nameList:
+            print(name.li.span.get_text()[:1])
+
+            #print('evaluationg if' + name.li.span.get_text() + ' <= ' + str(limitAmount))
+            if float(name.li.span.get_text().replace('$','')) <= limitAmount:
+                print('Match ->' + name.h3.a['href'])
+                itemsInQuery.append(name.h3.a['href'])
+    print("Found " + str(len(itemsInQuery)) + " items")
+    return itemsInQuery
+
+
+
+def bidOnItems(driver,searchItems,bidAmmount=0.06):
+    if len(searchItems) > 0:
+        for each in searchItems:        
+            driver.get(each)
+            try:
+                if not(driver.find_elements_by_xpath("//*[contains(text(), 're the highest bidder.')]")):
+                    driver.get(driver.find_element_by_id('bidBtn_btn').get_attribute("href"))
+                    if not(driver.find_elements_by_xpath("//*[contains(text(), 'Your max bid:')]")):
+                        driver.implicitly_wait(2)
+                        act = webdriver.ActionChains(driver)
+                        act.move_to_element(driver.find_element_by_id("MaxBidId")).click().send_keys(str(bidAmmount))
+                        act.perform()
+                        
+                        driver.implicitly_wait(2)
+                        act.move_to_element(driver.find_element_by_id("bidBtn_btn")).click()
+                        act.perform()   
+                       
+                        driver.implicitly_wait(2)
+                        act.move_to_element(driver.find_element_by_id("but_v4-1")).click()
+                        act.perform()                
+                        #driver.find_element_by_id("but_v4-1").click()
+
+                        driver.implicitly_wait(2)
+                        act.move_to_element(driver.find_element_by_id("but_v4-2")).click()
+                        act.perform()
+                        #driver.find_element_by_id("but_v4-2").click()
+            except Exception as e:
+                print(e)
+                pass
+                
+            if driver.find_elements_by_xpath("//*[contains(@id, 'but_v4-0')]"):
+                try:
+                    driver.find_element_by_id("but_v4-0").click()
+                except Exception as e:
+                    print(e)
+                    pass
