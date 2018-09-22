0495c5eefa3bae1e54a2f179ba94a2f480c01ec4
Bogomil Filipov <filipov.bogomil@gmail.com>
2018-09-22 19:11:34 +0300

    update script /PEP8 formating/

--- ebaySeleniumOperatons.py
+++ ebaySeleniumOperatons.py
@@ -1,71 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 22 16:16:56 2018
-
-@author: bFilipov
-"""
 from urllib.request import urlopen
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-def signin(driver,emailUsed,passswordUsed):
-    driver.get('https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=http%3A%2F%2Fwww.ebay.com%2F')
-    
-    action1 = webdriver.ActionChains(driver)
-    #action1.move_to_element(driver.find_element_by_css_selector('div #pri_signin div span input')).send_keys(emailUsed)''Stopped woringg 02.2017
-    #action1.move_to_element(driver.find_element_by_css_selector('div #pri_signin input')).send_keys(emailUsed) stopped working 10.2017
-    action1.move_to_element(driver.find_element_by_css_selector('input[placeholder="Email or username"]')).click().send_keys(emailUsed)
-    action1.perform()
-    action2 = webdriver.ActionChains(driver)
-    #action2.move_to_element(driver.find_element_by_css_selector('div #pri_signin :nth-child(6) span input')).click().send_keys(passswordUsed) stopped worlomg 10.2017
-    action2.move_to_element(driver.find_element_by_css_selector('input[type="Password"]')).click().send_keys(passswordUsed)
-    action2.perform()
-    driver.find_element_by_id('sgnBt').click()
 
-def findSearchItems(urls=[],limitAmount=0.05):
-    itemsInQuery = []
+def sign_in(driver, email_used, password_used):
+    driver.get('https://www.ebay.com/')
+    driver.find_element_by_css_selector("span[id=gh-ug] > a").click()
+
+    action1 = webdriver.ActionChains(driver)
+    action1.move_to_element(
+        driver.find_element_by_css_selector('input[id="userid"]')
+    ).click().send_keys(email_used)
+    action1.perform()
+
+    action2 = webdriver.ActionChains(driver)
+    action2.move_to_element(
+        driver.find_element_by_css_selector('input[id="pass"]')
+    ).click().send_keys(password_used)
+    action2.move_to_element(
+        driver.find_element_by_id('sgnBt')
+    ).click()
+    action2.perform()
+
+
+def find_items_from_list_with_value_less_than(urls, limit_amount=0.05):
+    items_in_query = []
     for item in urls:
-        html=urlopen(item)
-        bsObj = BeautifulSoup(html, "html.parser")
-        nameList = bsObj.findAll("li", {"class":"sresult lvresult clearfix li"})
-        #driver.get(item) commented on 10.2017 to speed things up
+        html = urlopen(item)
+        bs_obj = BeautifulSoup(html, "html.parser")
+        name_list = bs_obj.findAll("li", {"class":"sresult lvresult clearfix li"})
+        # driver.get(item) commented on 10.2017 to speed things up
         print(item)
 
-        for name in nameList:
+        for name in name_list:
             print(name.li.span.get_text()[:1])
 
-            #print('evaluationg if' + name.li.span.get_text() + ' <= ' + str(limitAmount))
-            if float(name.li.span.get_text().replace('$','')) <= limitAmount:
+            # print('evaluating if' + name.li.span.get_text() + ' <= ' + str(limit_amount))
+            if float(name.li.span.get_text().replace('$','')) <= limit_amount:
                 print('Match ->' + name.h3.a['href'])
-                itemsInQuery.append(name.h3.a['href'])
-    print("Found " + str(len(itemsInQuery)) + " items")
-    return itemsInQuery
+                items_in_query.append(name.h3.a['href'])
+    print("Found " + str(len(items_in_query)) + " items")
+    return items_in_query
 
 
-
-def bidOnItems(driver,searchItems,bidAmmount=0.06):
-    if len(searchItems) > 0:
-        for each in searchItems:        
+def bid_on_items(driver, search_items, bid_amount=0.06):
+    if len(search_items) > 0:
+        for each in search_items:
             driver.get(each)
             try:
-                #if not(driver.find_elements_by_xpath("//*[contains(text(), 're the highest bidder.')]")):
+                # if not(driver.find_elements_by_xpath("//*[contains(text(), 're the highest bidder.')]")):
                 driver.get(driver.find_element_by_xpath("//*[@id='bidBtn_btn']").get_attribute("href"))
-                #if not(driver.find_elements_by_xpath("//*[contains(text(), 'Your max bid:')]")):
+                # if not(driver.find_elements_by_xpath("//*[contains(text(), 'Your max bid:')]")):
                 driver.implicitly_wait(2)
                 act = webdriver.ActionChains(driver)
-                print(str(bidAmmount))
-                act.move_to_element(driver.find_element_by_xpath("//*[@id='maxbid']")).click().send_keys(str(bidAmmount))
+                print(str(bid_amount))
+                act.move_to_element(driver.find_element_by_xpath("//*[@id='maxbid']")).click().send_keys(str(bid_amount))
                 act.perform()
                  
                 driver.implicitly_wait(2)
                 act.move_to_element(driver.find_element_by_xpath("//*[@id='but_v4-1']")).click()
                 act.perform()                
-                #driver.find_element_by_id("but_v4-1").click()
+                # driver.find_element_by_id("but_v4-1").click()
 
                 driver.implicitly_wait(2)
                 act.move_to_element(driver.find_element_by_xpath("//*[@id='but_v4-2']")).click()
                 act.perform()
-                #driver.find_element_by_id("but_v4-2").click()
+                # driver.find_element_by_id("but_v4-2").click()
                 
                 driver.implicitly_wait(2)
                 act.move_to_element(driver.find_element_by_xpath("//*[@id='bidBtn_btn']")).click()
